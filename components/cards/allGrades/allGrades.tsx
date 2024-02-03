6bb56a69b1917949ac62cf705731702d89e902e2
Noah Streller <noah.streller@gmx.ch>
2024-02-03 11:15:51 +0100

    fix: clone cards

--- components/cards/allGrades/allGrades.tsx
+++ components/cards/allGrades/allGrades.tsx
@@ -1,91 +1,123 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
 import Grade from "@/lib/entities/grade";
 import { columns } from "./columns";
 import useTranslation from "next-translate/useTranslation";
 import { DataTable } from "./data-table";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuShortcut, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-import { Drawer, DrawerTrigger, DrawerContent, DrawerHeader, DrawerTitle, DrawerDescription, DrawerFooter, DrawerClose } from "@/components/ui/drawer";
-import {  Dialog, DialogTrigger, DialogContent, DialogTitle, DialogDescription, DialogHeader } from "@/components/ui/dialog";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuShortcut,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+	Drawer,
+	DrawerTrigger,
+	DrawerContent,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerDescription,
+	DrawerFooter,
+	DrawerClose,
+} from "@/components/ui/drawer";
+import {
+	Dialog,
+	DialogTrigger,
+	DialogContent,
+	DialogTitle,
+	DialogDescription,
+	DialogHeader,
+} from "@/components/ui/dialog";
 import { useState } from "react";
-import {isMobile} from 'react-device-detect';
+import { isMobile } from "react-device-detect";
 import { CreateGradeForm } from "@/components/create-grade-form";
 import Subjects from "@/lib/entities/subject";
 
-export function AllGrades({ data, setData, refresh }: { data: Grade[], setData: Function, refresh: Function}) {
-    const { t, lang } = useTranslation('common');
+export function AllGrades({
+	data,
+	setData,
+	refresh,
+}: {
+	data: Grade[];
+	setData: Function;
+	refresh: Function;
+}) {
+	const { t, lang } = useTranslation("common");
 
-    const [open, setOpen] = useState(false)
-    const isDesktop = !isMobile;
- 
-  if (isDesktop) {
-    return (
-        <Card>
-            <CardHeader>
-                <CardTitle>All Grades</CardTitle>
-                <CardDescription>All recent grades are listed here</CardDescription>
-                <Dialog open={open} onOpenChange={setOpen}>
-                    <DialogTrigger asChild>
-                    <Button>{t("grades.add")}</Button>
-                    </DialogTrigger>
-                    <DialogContent className="sm:max-w-[425px]">
-                    <DialogHeader>
-                        <DialogTitle>{t("grades.add")}</DialogTitle>
-                        <DialogDescription>{t("grades.add-desc")}</DialogDescription>
-                    </DialogHeader>
+	const [open, setOpen] = useState(false);
+	const isDesktop = !isMobile;
 
-                    <CreateGradeForm refresh={refresh} subjectSet={Subjects.get()} />
+	if (isDesktop) {
+		return (
+			<Card>
+				<CardHeader>
+					<CardTitle>{t("grades.all-grades")}</CardTitle>
+					<CardDescription>{t("grades.all-grades-desc")}</CardDescription>
+					<Dialog open={open} onOpenChange={setOpen}>
+						<DialogTrigger asChild>
+							<Button>{t("grades.add")}</Button>
+						</DialogTrigger>
+						<DialogContent className="sm:max-w-[425px]">
+							<DialogHeader>
+								<DialogTitle>{t("grades.add")}</DialogTitle>
+								<DialogDescription>
+									{t("grades.add-desc")}
+								</DialogDescription>
+							</DialogHeader>
 
-                    </DialogContent>
-                </Dialog>
+							<CreateGradeForm
+								refresh={refresh}
+								subjectSet={Subjects.get()}
+							/>
+						</DialogContent>
+					</Dialog>
+				</CardHeader>
+				<CardContent>
+					<DataTable columns={columns()} data={data} />
+				</CardContent>
+			</Card>
+		);
+	}
 
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>{t("grades.all-grades")}</CardTitle>
+				<CardDescription>{t("grades.all-grades-desc")}</CardDescription>
+				<Drawer open={open} onOpenChange={setOpen}>
+					<DrawerTrigger asChild>
+						<Button>{t("grades.add")}</Button>
+					</DrawerTrigger>
+					<DrawerContent>
+						<DrawerHeader className="text-left">
+							<DrawerTitle>{t("grades.add")}</DrawerTitle>
+							<DrawerDescription>{t("grades.add-desc")}</DrawerDescription>
+						</DrawerHeader>
 
-            </CardHeader>
-            <CardContent>
-                <DataTable columns={columns()} data={data} /> 
-            </CardContent>
-        </Card>
-    )
-  }
- 
-  return (
-    <Drawer open={open} onOpenChange={setOpen}>
-      <DrawerTrigger asChild>
-        <Button variant="outline">Edit Profile</Button>
-      </DrawerTrigger>
-      <DrawerContent>
-        <DrawerHeader className="text-left">
-          <DrawerTitle>Edit profile</DrawerTitle>
-          <DrawerDescription>
-            Make changes to your profile here. Click save when youre done.
-          </DrawerDescription>
-        </DrawerHeader>
-        <CreateGradeForm refresh={refresh} subjectSet={Subjects.get()} />
-        <DrawerFooter className="pt-2">
-          <DrawerClose asChild>
-            <Button variant="outline">Cancel</Button>
-          </DrawerClose>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
-  )
+						<CreateGradeForm refresh={refresh} subjectSet={Subjects.get()} />
+
+						<DrawerFooter className="pt-2">
+							<DrawerClose asChild>
+								<Button variant="outline">Cancel</Button>
+							</DrawerClose>
+						</DrawerFooter>
+					</DrawerContent>
+				</Drawer>
+			</CardHeader>
+			<CardContent>
+				<DataTable columns={columns()} data={data} />
+			</CardContent>
+		</Card>
+	);
 }
-
-//     return (
-//         <Card>
-//             <CardHeader>
-//                 <CardTitle>All Grades</CardTitle>
-//                 <CardDescription>All recent grades are listed here</CardDescription>
-//                 <Button onClick={() => {new Grade(Math.random() + 3.5, "Naturwissenschaften und Technik"); refresh();}}>{t("grades.add") + " nt"}</Button>
-//                 <Button onClick={() => {new Grade(Math.random() + 3.5, "Mathematik"); refresh();}}>{t("grades.add") + " ma"}</Button>
-//                 <Button onClick={() => {new Grade(Math.random() + 3.5, "Englisch"); refresh();}}>{t("grades.add") + " en"}</Button>
-
-
-//             </CardHeader>
-//             <CardContent>
-//                 <DataTable columns={columns()} data={data} /> 
-//             </CardContent>
-//         </Card>
-//     )
-// }
---
+++ components/create-subject-form.tsx
@@ -0,0 +1,154 @@
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Check, ChevronsUpDown } from "lucide-react"
+import { useForm } from "react-hook-form"
+import { string, z } from "zod"
+
+import { cn, truncateText } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import useTranslation from "next-translate/useTranslation"
+import { useEffect, useState } from "react"
+import { toast } from "sonner"
+import Grade from "@/lib/entities/grade"
+import { Input } from "./ui/input"
+import appGlobals from "@/lib/app.globals"
+import { ScrollArea } from "./ui/scroll-area"
+
+export function CreateSubjectForm({subjectSet, refresh} : {subjectSet: Set<string>, refresh: Function}) {
+    const { t, lang } = useTranslation('common');
+
+    const initial: string[] = [];
+
+    const [subjects, setSubjects] = useState(initial);
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        subjectSet.forEach(subj => {
+            setSubjects(subjects => [...subjects, subj])
+        });
+    }, [subjectSet]);
+    
+    const FormSchema = z.object({
+        subject: z.string({
+            required_error: t("errors.required"),
+        }),
+        grade: z.number({
+            invalid_type_error: t("errors.invalid-type.number"),
+            required_error: t("errors.required"),
+        }).gte(appGlobals.minimumGrade).lte(appGlobals.maximumGrade),
+    })
+
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+    })
+
+    function onSubmit(data: z.infer<typeof FormSchema>) {
+        const gradeAsNumber = Number(data.grade);
+        new Grade(gradeAsNumber, data.subject);
+        toast(t("grades.add-success"));
+        refresh();
+    }
+
+    return (
+        <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 m-5">
+            <FormField
+            control={form.control}
+            name="subject"
+            render={({ field }) => (
+                <FormItem className="flex flex-col">
+                <FormLabel>Subject</FormLabel>
+                <Popover open={open} onOpenChange={setOpen}>
+                    <PopoverTrigger asChild>
+                    <FormControl>
+                        <Button
+                        variant="outline"
+                        role="combobox"
+                        className={cn(
+                            "w-[200px] justify-between",
+                            !field.value && "text-muted-foreground"
+                        )}
+                        >
+                        {field.value
+                        ? truncateText(subjects.find((subject) => subject === field.value) ?? "", 20).text
+                        : "Select subject"}
+                        <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                        </Button>
+                    </FormControl>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-[200px] p-0">
+                    <Command>
+                        <CommandInput placeholder={t("subjects.search")} />
+                        <CommandEmpty>No subject found.</CommandEmpty>
+                        <ScrollArea className="max-h-[30dvh] overflow-y-auto">
+                            <CommandGroup>
+                            {subjects.map((subject) => (
+                                <CommandItem
+                                value={subject}
+                                key={subject}
+                                onSelect={() => {
+                                    form.setValue("subject", subject);
+                                    setOpen(false)
+                                }}
+                                >
+                                <Check
+                                    className={cn(
+                                    "mr-2 h-4 w-4",
+                                    subject === field.value
+                                        ? "opacity-100"
+                                        : "opacity-0"
+                                    )}
+                                />
+                                {subject}
+                                </CommandItem>
+                            ))}
+                            </CommandGroup>
+                        </ScrollArea>
+                    </Command>
+                    </PopoverContent>
+                </Popover>
+                <FormMessage />
+                </FormItem>
+            )}
+            />
+            
+            <FormField
+            control={form.control}
+            name="grade"
+            render={({ field }) => (
+                <FormItem>
+                <FormLabel>Grade</FormLabel>
+                <FormControl>
+                    <Input type="number" step="any" placeholder={t("grades.add-placeholder")} {...field} onChange={(e) => field.onChange(Number(e.target.value))} />
+                </FormControl>
+                <FormMessage />
+                </FormItem>
+            )}
+            />
+
+            <Button className="w-full" type="submit">Submit</Button>
+        </form>
+        </Form>
+    )
+}
