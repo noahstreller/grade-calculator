970a26d617af224c1f069027842733672976c948
Noah Streller <65013011+noahstreller@users.noreply.github.com>
2024-02-03 22:47:42 +0000

    fix: hope this works

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -14,9 +14,9 @@ export default function HeaderComponent() {
             <Link href={"/"} className="flex items-center text-center opacity-80 gap-1 hover:opacity-100 hover:translate-x-[10px] transition-all">
                 <img src="/icon.png" alt="Logo" className="h-[100%] dark:invert opacity-80 drag-none select-none" />
                 {
-                !isMobileDevice()
-                ? <h1 className={"text-3xl font-bold text-foreground whitespace-nowrap select-none"}>{t('app.title')}</h1>
-                : null
+                isMobileDevice()
+                ? null
+                : <h1 className={"text-3xl font-bold text-foreground whitespace-nowrap select-none"}>{t('app.title')}</h1>
                 }
             </Link>
             <div className="flex">
