1ca62a906c98a549fd07c3de9caf06c224ec9724
Noah Streller <noah.streller@gmx.ch>
2024-02-04 17:42:13 +0100

    refactor: rely on tailwind for cardboard

---
+++ components/ui/cardboard.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from 'react';
+
+export function CardBoard({ children, row, className = "" }: { children: ReactNode, row?: boolean, className?: string}) {
+    const cardboardClasses = "flex items-start justify-center gap-5 [&>*]:w-[100%] [&>*]:max-w-[90vw]";
+
+    if (row) {
+        return (
+            <div className={`flex-row ${cardboardClasses} ${className}`}>
+                {children}
+            </div>
+        )
+    }
+    return (
+        <div className={`flex-col ${cardboardClasses} ${className}`}>
+            {children}
+        </div>
+    )
+}
