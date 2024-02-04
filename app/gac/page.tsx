abc5b1dd951422e488f05897b535631fe9e80080
Noah Streller <noah.streller@gmx.ch>
2024-02-04 17:42:13 +0100

    refactor: rely on tailwind for cardboard

--- app/gac/page.tsx
+++ app/gac/page.tsx
@@ -4,11 +4,10 @@ import { AllSubjects } from '@/components/cards/allSubjects/allSubjects';
 import { CardSkeleton } from '@/components/cards/card-skeleton';
 import FailingGradesCard from '@/components/cards/failingGradesCard/failingGradesCard';
 import PassingGradesCard from '@/components/cards/passingGradesCard/passingGradesCard';
-import { CardBoard } from '@/components/ui/cardboard/cardboard';
+import { CardBoard } from '@/components/ui/cardboard';
 import Grade from '@/lib/entities/grade';
 import { GradeAverage } from '@/lib/entities/gradeAverage';
 import Subjects from '@/lib/entities/subject';
-import { isMobileDevice } from '@/lib/utils';
 import { useEffect, useState } from 'react';
 
 export default function GradeAverageCalculator() {
@@ -55,16 +54,37 @@ export default function GradeAverageCalculator() {
     
 
 
+    // return (
+    //     loaded 
+    //     ? isMobileDevice()
+    //         ? <CardBoard>
+    //             <PassingGradesCard data={passingData} setData={setPassingData} />
+    //             <FailingGradesCard data={failingData} setData={setFailingData} />
+    //             <AllSubjects data={subjectData} setData={setSubjectData} refresh={refreshAll} />
+    //             <AllGrades data={gradeData} setData={setGradeData} refresh={refreshAll} />
+    //         </CardBoard>
+    //         : <CardBoard row className=''>
+    //             <CardBoard>
+    //                 <PassingGradesCard data={passingData} setData={setPassingData} />
+    //                 <FailingGradesCard data={failingData} setData={setFailingData} />
+    //             </CardBoard>
+    //             <AllSubjects data={subjectData} setData={setSubjectData} refresh={refreshAll} />
+    //             <AllGrades data={gradeData} setData={setGradeData} refresh={refreshAll} />
+    //         </CardBoard>
+    //     : <CardBoard row>
+    //         <CardBoard>
+    //             <CardSkeleton variant='small' />
+    //             <CardSkeleton variant='small' />
+    //         </CardBoard>
+    //         <CardSkeleton variant='medium' />
+    //         <CardSkeleton variant='large' />
+    //     </CardBoard>    
+    // );
+
     return (
         loaded 
-        ? isMobileDevice()
-            ? <CardBoard>
-                <PassingGradesCard data={passingData} setData={setPassingData} />
-                <FailingGradesCard data={failingData} setData={setFailingData} />
-                <AllSubjects data={subjectData} setData={setSubjectData} refresh={refreshAll} />
-                <AllGrades data={gradeData} setData={setGradeData} refresh={refreshAll} />
-            </CardBoard>
-            : <CardBoard row>
+        ? <>
+            <CardBoard row>
                 <CardBoard>
                     <PassingGradesCard data={passingData} setData={setPassingData} />
                     <FailingGradesCard data={failingData} setData={setFailingData} />
@@ -72,6 +92,7 @@ export default function GradeAverageCalculator() {
                 <AllSubjects data={subjectData} setData={setSubjectData} refresh={refreshAll} />
                 <AllGrades data={gradeData} setData={setGradeData} refresh={refreshAll} />
             </CardBoard>
+        </>
         : <CardBoard row>
             <CardBoard>
                 <CardSkeleton variant='small' />
