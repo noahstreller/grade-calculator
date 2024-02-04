73920a8e4cd597dbc40313ba22c6e941f12e5cfd
Noah Streller <noah.streller@gmx.ch>
2024-02-04 17:05:26 +0100

    refactor: remove css and use tailwind

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
+import HeaderComponent from '@/components/header'
+import { ThemeProvider } from '@/components/theme-provider'
+import { cn } from '@/lib/utils'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import '../styles/globals.css'
-import HeaderComponent from '@/components/header/header'
-import { cn } from '@/lib/utils'
 import { Toaster } from 'sonner'
-import { ThemeProvider } from '@/components/theme-provider'
+import '../styles/globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
--- components/header.tsx
+++ components/header.tsx
@@ -1,13 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 import useTranslation from "next-translate/useTranslation";
-import { ThemeSwitcher } from "../theme-switcher";
-import { ClearDataButton } from "../ui/clear-data-button";
-import styles from './header.module.css';
+import { ThemeSwitcher } from "./theme-switcher";
+import { ClearDataButton } from "./ui/clear-data-button";
 
 export default function HeaderComponent() {
     const { t, lang } = useTranslation('common');
     return (
-        <header className={styles.appHeader}>
+        <header className="h-[64px] py-[10px] px-[20px] flex justify-between fixed top-0 left-0 w-[100dvw] bg-transparent z-[1000] backdrop-blur-[20px]">
             <div className="flex items-center text-center opacity-80 gap-1 hover:opacity-100 hover:translate-x-[10px] transition-all">
                 <img src="/icon.png" alt="Logo" className="h-[100%] dark:invert opacity-80 drag-none select-none" />
                 <h1 className={"hidden md:inline-block xl:inline-block lg:inline-block sm:inline-block text-3xl font-bold text-foreground whitespace-nowrap select-none"}>{t('app.title')}</h1>
