319402e82cd81f4c5d6212616992c2c8aa05befc
Noah Streller <noah.streller@gmx.ch>
2024-03-13 11:39:15 +0100

    feat: add pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextTranslate = require('next-translate-plugin')
+const withSerwist = require("@serwist/next").default({
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+});
 
-module.exports = nextTranslate({
-    reactStrictMode: false,
-})
+module.exports = withSerwist(
+    nextTranslate({
+        reactStrictMode: false,
+    })
+);
