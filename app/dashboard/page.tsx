5893ed1703c1a840b5a808bb984da7dbb0d3b34e
Noah Streller <noah.streller@gmx.ch>
2024-04-25 11:36:29 +0200

    fix: exclude dashboad

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,7 @@ import Dashboard from "@/components/pages/dashboard";
 export default function DashboardPage() {
   return (
     <div>
+      <meta name="robots" content="noindex,nofollow" />
       <Dashboard />
     </div>
   );
