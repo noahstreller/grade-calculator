e2d89551c6d646fef68c0e6a97e5d17aeb051630
Noah Streller <noah.streller@gmx.ch>
2024-05-01 18:08:39 +0200

    fix: colored grade has a classname prop now

--- components/colored-grade.tsx
+++ components/colored-grade.tsx
@@ -6,7 +6,7 @@ import { round } from "@/lib/utils";
 import { Empty } from "@/types/types";
 import { useEffect, useState } from "react";
 
-export function ColoredGrade({grade, noMargin}: {grade: number | Empty, noMargin?: boolean}) {
+export function ColoredGrade({grade, className, roundDecimals}: {grade: number | Empty, className?: string, roundDecimals?: number}) {
 
   const [isPassing, setIsPassing] = useState<boolean | null>(null);
   const preferences = usePreferences();
@@ -26,15 +26,39 @@ export function ColoredGrade({grade, noMargin}: {grade: number | Empty, noMargin
 
   return (
     <>
-      {isPassing === null && <p className={noMargin ? "text-muted-foreground" : "text-muted-foreground ml-4"}>-</p>}
+      {isPassing === null && (
+        <p
+          className={
+            className
+              ? `${className} text-muted-foreground`
+              : "text-muted-foreground ml-4"
+          }
+        >
+          -
+        </p>
+      )}
       {isPassing === false && (
-        <p className="text-red-400 ml-4">
-          {round(grade ?? 0, preferences.preferences?.gradeDecimals!)}
+        <p
+          className={
+            className ? `${className} text-red-400` : "text-red-400 ml-4"
+          }
+        >
+          {round(
+            grade ?? 0,
+            roundDecimals ?? preferences.preferences?.gradeDecimals!
+          )}
         </p>
       )}
       {isPassing === true && (
-        <p className="text-green-400 ml-4">
-          {round(grade ?? 0, preferences.preferences?.gradeDecimals!)}
+        <p
+          className={
+            className ? `${className} text-green-400` : "text-green-400 ml-4"
+          }
+        >
+          {round(
+            grade ?? 0,
+            roundDecimals ?? preferences.preferences?.gradeDecimals!
+          )}
         </p>
       )}
     </>
