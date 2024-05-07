8907ed2865ab5309590d546545cf1974049fe3c7
Noah Streller <noah.streller@gmx.ch>
2024-05-07 15:07:03 +0200

    fix: added export for empty subjects

--- lib/services/export-service.ts
+++ lib/services/export-service.ts
@@ -4,23 +4,37 @@ import {
   GradeWithSubject,
   NewGradeWithNewSubject,
   NewPreferences,
+  NewSubject,
   Preferences,
+  Subject,
 } from "@/db/schema";
 import { catchProblem } from "@/lib/problem";
-import { addGrade, getAllGradesWithSubject } from "@/lib/services/grade-service";
-import { getPreferences, savePreferences } from "@/lib/services/preferences-service";
-import { addSubject, getSubjectIdElseAdd } from "@/lib/services/subject-service";
+import {
+  addGrade,
+  getAllGradesWithSubject,
+} from "@/lib/services/grade-service";
+import {
+  getPreferences,
+  savePreferences,
+} from "@/lib/services/preferences-service";
+import {
+  addSubject,
+  getAllSubjects,
+  getSubjectIdElseAdd,
+} from "@/lib/services/subject-service";
 
 export type ExportableData = {
   preferences: NewPreferences | undefined;
-  data: NewGradeWithNewSubject[];
+  grades: NewGradeWithNewSubject[];
+  subjects: NewSubject[];
 };
 
 export async function prepareDataForExport(): Promise<ExportableData> {
   const preferences: Preferences = catchProblem(await getPreferences())[0];
-  const data: GradeWithSubject[] = catchProblem(
-    await getAllGradesWithSubject()
+  const grades: GradeWithSubject[] = catchProblem(
+    await getAllGradesWithSubject(),
   );
+  const subjects: Subject[] = catchProblem(await getAllSubjects());
 
   const strippedPreferences = (): NewPreferences | undefined => {
     if (preferences)
@@ -36,7 +50,7 @@ export async function prepareDataForExport(): Promise<ExportableData> {
     return undefined;
   };
 
-  const strippedData: NewGradeWithNewSubject[] = data.map((grade) => {
+  const strippedGrades: NewGradeWithNewSubject[] = grades.map((grade) => {
     return {
       grades: {
         value: grade.grades.value,
@@ -49,31 +63,50 @@ export async function prepareDataForExport(): Promise<ExportableData> {
     };
   });
 
+  const strippedSubjects: NewSubject[] = subjects.map((subject) => {
+    return {
+      name: subject.name,
+    };
+  });
+
   const exportableData = {
     preferences: strippedPreferences(),
-    data: strippedData,
+    grades: strippedGrades,
+    subjects: strippedSubjects,
   };
   return exportableData;
 }
 
 export async function importData(data: ExportableData, purge: boolean) {
   if (data.preferences) savePreferences(data.preferences);
-  const nonUniqueSubjects = data.data.map((grade) => grade.subjects.name);
+  const nonUniqueSubjectsFromGrades = data.grades.map(
+    (grade) => grade.subjects.name,
+  );
+  const nonUniqueSubjectsFromSubjects = data.subjects.map(
+    (subject) => subject.name,
+  );
+  const nonUniqueSubjects = [
+    ...nonUniqueSubjectsFromGrades,
+    ...nonUniqueSubjectsFromSubjects,
+  ];
   const uniqueSubjects = [...new Set(nonUniqueSubjects)];
-  const subjectWithIds = await Promise.all(uniqueSubjects.map(async (subjectName) => {
-    const subject = { name: subjectName };
-    if(purge) {
-      let result = catchProblem(await addSubject(subject));
-      return { name: subject.name, id: result };
-    }
-    else {
-      let result = catchProblem(await getSubjectIdElseAdd(subject));
-      return { name: subject.name, id: result };
-    }
-  }));
+  const subjectWithIds = await Promise.all(
+    uniqueSubjects.map(async (subjectName) => {
+      const subject = { name: subjectName };
+      if (purge) {
+        let result = catchProblem(await addSubject(subject));
+        return { name: subject.name, id: result };
+      } else {
+        let result = catchProblem(await getSubjectIdElseAdd(subject));
+        return { name: subject.name, id: result };
+      }
+    }),
+  );
 
-  let results = data.data.forEach(async (grade) => {
-    let resultingSubject = subjectWithIds.find((subject) => subject.name === grade.subjects.name)?.id;
+  let results = data.grades.forEach(async (grade) => {
+    let resultingSubject = subjectWithIds.find(
+      (subject) => subject.name === grade.subjects.name,
+    )?.id;
     grade.grades.subject_fk = resultingSubject;
     grade.grades.date = new Date(grade.grades.date || Date.now());
     let result = catchProblem(await addGrade(grade.grades));
