21116502b87c63d0146a507561c818ad6d3d87c8
Noah Streller <noah.streller@gmx.ch>
2024-05-08 17:54:53 +0200

    feat: integrate the grade description

--- components/subject-grade-badge.tsx
+++ components/subject-grade-badge.tsx
@@ -1,19 +1,41 @@
-"use client"
+"use client";
 import { Badge } from "@/components/ui/badge";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { truncateText } from "@/lib/utils";
 import { AverageWithSubject } from "@/types/types";
 import useTranslation from "next-translate/useTranslation";
 
-export const SubjectGradeBadge = ({average, className, hideText = false}: {average: AverageWithSubject, className?: string, hideText?: boolean}) => {
+export const SubjectGradeBadge = ({
+  average,
+  className,
+  hideText = false,
+}: {
+  average: AverageWithSubject;
+  className?: string;
+  hideText?: boolean;
+}) => {
   const { t } = useTranslation("common");
   const averageGrade = average.average?.gradeAmount ?? 0;
   const truncatedGrade = truncateText(averageGrade.toString(), 3).text;
   return (
-    <Badge variant="secondary" className={`${className}`}>
-      {`${truncatedGrade} ${ hideText ? "" : t("grades.grades", {count: average.average?.gradeAmount})}`}
-    </Badge>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger className={className}>
+          <Badge variant="secondary" className={className}>
+            {`${truncatedGrade} ${
+              hideText
+                ? ""
+                : t("grades.grades", { count: average.average?.gradeAmount })
+            }`}
+          </Badge>
+        </TooltipTrigger>
+        <TooltipContent>Grade Count</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
-}
-
-
-
+};
