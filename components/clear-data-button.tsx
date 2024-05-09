c4d7dc2a5dbac8782f0beefb310b0e9983de895d
Noah Streller <noah.streller@gmx.ch>
2024-05-10 01:59:11 +0200

    feat: option to clear grades only

--- components/clear-data-button.tsx
+++ components/clear-data-button.tsx
@@ -1,11 +1,14 @@
 "use client";
-import { clearUserSubjectsGrades } from "@/lib/services/user-service";
+import { Button } from "@/components/ui/button";
+import {
+  clearUserGrades,
+  clearUserSubjectsGrades,
+} from "@/lib/services/user-service";
 import { ClearDataTranslations } from "@/lib/translationObjects";
 import { useSession } from "next-auth/react";
+import { useState } from "react";
 import {
   AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
   AlertDialogContent,
   AlertDialogDescription,
   AlertDialogFooter,
@@ -16,23 +19,27 @@ import {
 
 export function ClearDataButton({
   translations,
-  children
+  children,
 }: {
   translations: ClearDataTranslations;
   children: React.ReactNode;
 }) {
+  const [open, setOpen] = useState(false);
+
   function clearData() {
     clearUserSubjectsGrades();
+    window.location.reload();
+  }
+  function clearGrades() {
+    clearUserGrades();
     window.location.reload();
   }
   const session = useSession();
 
   return session.status === "authenticated" ? (
     <div>
-      <AlertDialog>
-        <AlertDialogTrigger asChild>
-          {children}
-        </AlertDialogTrigger>
+      <AlertDialog open={open} onOpenChange={setOpen}>
+        <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
         <AlertDialogContent>
           <AlertDialogHeader>
             <AlertDialogTitle>{translations.prompt}</AlertDialogTitle>
@@ -40,11 +47,16 @@ export function ClearDataButton({
               {translations.message}
             </AlertDialogDescription>
           </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>{translations.cancel}</AlertDialogCancel>
-            <AlertDialogAction onClick={clearData}>
+          <AlertDialogFooter className="flex flex-col-reverse gap-3">
+            <Button onClick={() => setOpen(false)}>
+              {translations.cancel}
+            </Button>
+            <Button onClick={clearGrades} variant="secondary">
+              Clear grades only
+            </Button>
+            <Button variant="destructive" onClick={clearData}>
               {translations.dangerContinue}
-            </AlertDialogAction>
+            </Button>
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
