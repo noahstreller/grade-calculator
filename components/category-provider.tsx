4177454264eb75ad4a7b933742b04874c8a5b9b9
Noah Streller <noah.streller@gmx.ch>
2024-05-14 06:56:17 +0200

    wip: continuation combobox

---
+++ components/category-provider.tsx
@@ -0,0 +1,52 @@
+"use client";
+import { Category } from "@/db/schema";
+import { catchProblem } from "@/lib/problem";
+import { getPreferencesElseGetDefault } from "@/lib/services/preferences-service";
+import { useSession } from "next-auth/react";
+import { createContext, useContext, useEffect, useState } from "react";
+
+type CategoryContextType = {
+  category: Category | undefined;
+  setCategory: (category: Category) => void;
+  loading: boolean;
+};
+
+const defaultContextValue: CategoryContextType = {
+  category: undefined,
+  setCategory: () => void 0,
+  loading: true,
+};
+
+const CategoryContext = createContext(defaultContextValue);
+
+export function CategoryProvider({ children }: { children: React.ReactNode }) {
+  const [category, setCategory] = useState<Category>({
+    id: 0,
+    name: "",
+    selected: false,
+    userId: "",
+
+    //todo
+  });
+  const [loading, setLoading] = useState(true);
+  const session = useSession();
+
+  useEffect(() => {
+    if (session.status === "authenticated") {
+      getPreferencesElseGetDefault().then((result): void => {
+        setCategory(catchProblem(result));
+        setLoading(false);
+      });
+    }
+  }, [session]);
+
+  return (
+    <CategoryContext.Provider
+      value={{ category: category, setCategory: setCategory, loading }}
+    >
+      {children}
+    </CategoryContext.Provider>
+  );
+}
+
+export const usePreferences = () => useContext(CategoryContext);
