5070879a9a219ed5513d469ff470509288e5c319
Noah Streller <noah.streller@gmx.ch>
2024-05-14 15:08:38 +0200

    feat: combobox working

--- components/category-provider.tsx
+++ components/category-provider.tsx
@@ -1,34 +1,33 @@
 "use client";
 import { Category } from "@/db/schema";
 import { catchProblem } from "@/lib/problem";
-import { getCurrentCategoryElseInsert } from "@/lib/services/category-service";
-import { getPreferencesElseGetDefault } from "@/lib/services/preferences-service";
+import {
+  getAllCategories,
+  getCurrentCategoryElseInsert,
+  selectCategory,
+} from "@/lib/services/category-service";
 import { useSession } from "next-auth/react";
 import { createContext, useContext, useEffect, useState } from "react";
 
 type CategoryContextType = {
   category: Category | undefined;
   setCategory: (category: Category) => void;
+  categories: Category[];
   loading: boolean;
 };
 
 const defaultContextValue: CategoryContextType = {
   category: undefined,
   setCategory: () => void 0,
+  categories: [],
   loading: true,
 };
 
 const CategoryContext = createContext(defaultContextValue);
 
 export function CategoryProvider({ children }: { children: React.ReactNode }) {
-  const [category, setCategory] = useState<Category>({
-    id: 0,
-    name: "",
-    selected: false,
-    userId: "",
-
-    //todo
-  });
+  const [category, setCategory] = useState<Category>();
+  const [categories, setCategories] = useState<Category[]>([]);
   const [loading, setLoading] = useState(true);
   const session = useSession();
 
@@ -41,9 +40,28 @@ export function CategoryProvider({ children }: { children: React.ReactNode }) {
     }
   }, [session]);
 
+  useEffect(() => {
+    if (session.status === "authenticated") {
+      getAllCategories().then((result) => {
+        setCategories([...catchProblem(result)]);
+      });
+    }
+  }, [session]);
+
+  useEffect(() => {
+    if (session.status === "authenticated") {
+      if (category?.id) selectCategory(category.id);
+    }
+  }, [category, session]);
+
   return (
     <CategoryContext.Provider
-      value={{ category: category, setCategory: setCategory, loading }}
+      value={{
+        category: category,
+        setCategory: setCategory,
+        loading,
+        categories,
+      }}
     >
       {children}
     </CategoryContext.Provider>
