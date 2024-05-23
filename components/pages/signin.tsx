2f833d438507c044a1eed2928cd9ac206b42cb55
Noah Streller <noah.streller@gmx.ch>
2024-05-23 11:53:36 +0200

    feat: update login page

--- components/pages/signin.tsx
+++ components/pages/signin.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { EmailLoginForm } from "@/components/email-login-form";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -19,16 +20,17 @@ import { Highlight } from "@/components/ui/card-stack";
 import { Separator } from "@/components/ui/separator";
 import { LoadingSpinner } from "@/components/ui/spinner";
 import { SiDiscord, SiGithub, SiGoogle } from "@icons-pack/react-simple-icons";
-import { Clock, Container, Globe, MailCheck } from "lucide-react";
+import { Bird, Clock, Container, Globe, MailCheck } from "lucide-react";
 import { signIn, useSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export function SignInPageComponent() {
   const session = useSession();
-  const sent = useSearchParams().get("sent");
   const router = useRouter();
+  const params = useSearchParams();
+  const [error, setError] = useState<string | null>(params.get("error"));
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -55,66 +57,68 @@ export function SignInPageComponent() {
           </BreadcrumbItem>
         </BreadcrumbList>
       </Breadcrumb>
-      {}
 
       {session.status === "unauthenticated" ? (
-        <Card className="transition-all">
-          <CardHeader>
-            <CardTitle>Sign In</CardTitle>
-            <CardDescription>
-              Sign in to your account using one of the providers below.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="gap-4 flex flex-col">
-            {process.env.NODE_ENV === "development" && (
+        <>
+          {error && <AuthError error={error} />}
+          <Card className="transition-all">
+            <CardHeader>
+              <CardTitle>Sign In</CardTitle>
+              <CardDescription>
+                Sign in to your account using one of the providers below.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="gap-4 flex flex-col">
+              {process.env.NODE_ENV === "development" && (
+                <Button
+                  variant={"secondary"}
+                  className="w-full"
+                  onClick={() => {
+                    signIn("local");
+                  }}
+                >
+                  <Container className="m-2 size-5" /> Local
+                </Button>
+              )}
               <Button
-                variant={"secondary"}
                 className="w-full"
                 onClick={() => {
-                  signIn("local");
+                  signIn("discord");
                 }}
               >
-                <Container className="m-2 size-5" /> Local
+                <SiDiscord className="m-2 size-5" /> Discord
               </Button>
-            )}
-            <Button
-              className="w-full"
-              onClick={() => {
-                signIn("discord");
-              }}
-            >
-              <SiDiscord className="m-2 size-5" /> Discord
-            </Button>
-            <Button
-              className="w-full"
-              onClick={() => {
-                signIn("github");
-              }}
-            >
-              <SiGithub className="m-2 size-5" /> GitHub
-            </Button>
-            <Button
-              className="w-full"
-              onClick={() => {
-                signIn("google");
-              }}
-            >
-              <SiGoogle className="m-2 size-5" /> Google
-            </Button>
-            <Separator />
-            <EmailLoginForm />
-            <CardDescription>
-              Check out the legacy version, if you prefer saving your data
-              locally.
-            </CardDescription>
-            <Button variant={"secondary"} asChild>
-              <Link href="https://legacy.grades.nstr.dev">
-                <Globe className="size-4 mr-2" />
-                Legacy Version
-              </Link>
-            </Button>
-          </CardContent>
-        </Card>
+              <Button
+                className="w-full"
+                onClick={() => {
+                  signIn("github");
+                }}
+              >
+                <SiGithub className="m-2 size-5" /> GitHub
+              </Button>
+              <Button
+                className="w-full"
+                onClick={() => {
+                  signIn("google");
+                }}
+              >
+                <SiGoogle className="m-2 size-5" /> Google
+              </Button>
+              <Separator />
+              <EmailLoginForm />
+              <CardDescription>
+                Check out the legacy version, if you prefer saving your data
+                locally.
+              </CardDescription>
+              <Button variant={"secondary"} asChild>
+                <Link href="https://legacy.grades.nstr.dev">
+                  <Globe className="size-4 mr-2" />
+                  Legacy Version
+                </Link>
+              </Button>
+            </CardContent>
+          </Card>
+        </>
       ) : (
         <Card className="transition-all">
           <CardHeader>
@@ -134,23 +138,66 @@ export function SignInPageComponent() {
 
 export function SentEmailComponent() {
   return (
-    <Card className="transition-all">
-      <CardHeader>
-        <CardTitle>E-Mail sign-in</CardTitle>
-        <CardDescription>
-          Make sure to check your spam folder, if you did not receive it.
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="flex flex-col justify-center items-center gap-4">
-        <div className="flex flex-row items-center gap-2">
-          <MailCheck className="size-5" /> Your{" "}
-          <Highlight>magic link</Highlight> is on the way! Check your Inbox.
-        </div>
-        <p className="text-muted-foreground flex flex-row gap-2 items-center">
-          <Clock className="size-5" />
-          It may take a moment to arrive.
-        </p>
-      </CardContent>
-    </Card>
+    <div className="flex flex-col gap-5">
+      <Breadcrumb className="pl-2">
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/">Home</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/login">Login</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/login/sent">Sent</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <Card className="transition-all">
+        <CardHeader>
+          <CardTitle>E-Mail sign-in</CardTitle>
+          <CardDescription>
+            Make sure to check your spam folder, if you did not receive it.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-col justify-center items-center gap-4">
+          <div className="flex flex-row items-center gap-2">
+            <MailCheck className="size-5" /> Your{" "}
+            <Highlight>magic link</Highlight> is on the way! Check your Inbox.
+          </div>
+          <p className="text-muted-foreground flex flex-row gap-2 items-center">
+            <Clock className="size-5" />
+            It may take a moment to arrive.
+          </p>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+function AuthError({ error }: { error: string }) {
+  return (
+    <Alert className="border-red-400 border-2 shadow-lg">
+      <Bird className="size-4" />
+      <div className="flex flex-col gap-2">
+        <AlertTitle>Something broke.</AlertTitle>
+        <Separator />
+        <AlertDescription className="space-y-1">
+          Please try again, use a different account or contact{" "}
+          <Button asChild variant={"link"} className="m-0 p-0 h-fit font-bold">
+            <Link href="mailto:dev@nstr.dev">dev@nstr.dev</Link>
+          </Button>{" "}
+          if the issue persists.
+          <p className="text-muted-foreground font-mono">Error Code: {error}</p>
+        </AlertDescription>
+      </div>
+    </Alert>
   );
 }
