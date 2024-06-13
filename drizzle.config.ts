ab9e63472bc46c9a961925ec0d47fbe05da6fc83
Noah Streller <noah.streller@gmx.ch>
2024-06-14 00:15:36 +0200

    feat: coolify additions

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,14 @@
 import * as dotenv from "dotenv";
 import type { Config } from "drizzle-kit";
 
-dotenv.config({ path: ".env.development.local" });
+process.env.VERCEL_ENV === "development" &&
+  dotenv.config({ path: ".env.development.local" });
 
 export default {
   schema: "./db/schema.ts",
   out: "./drizzle",
   driver: "pg", // 'pg' | 'mysql2' | 'better-sqlite' | 'libsql' | 'turso'
   dbCredentials: {
-    host: process.env.POSTGRES_HOST!,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE!,
-    ssl: process.env.VERCEL_ENV === "development" ? false : true,
+    connectionString: process.env.POSTGRES_URL ?? "",
   },
 } satisfies Config;
