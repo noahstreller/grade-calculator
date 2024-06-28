4d5269e6b1c601462d6862a6330e9c38991af463
Noah Streller <noah.streller@gmx.ch>
2024-06-28 08:35:35 +0200

    chore: add umami env to legacy

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,13 +90,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      {process.env.NODE_ENV === "production" && (
-        <Script
-          defer
-          src="https://cloud.umami.is/script.js"
-          data-website-id="3f2cf0c8-48d4-42da-b230-96f70475a4d6"
-        />
-      )}
+      {process.env.NODE_ENV === "production" &&
+        process.env.UMAMI_SCRIPT_URL &&
+        process.env.UMAMI_DATA_WEBSITE_ID && (
+          <Script
+            defer
+            src={process.env.UMAMI_SCRIPT_URL}
+            data-website-id={process.env.UMAMI_DATA_WEBSITE_ID}
+          />
+        )}
       <body className={cn(inter.className)}>
         <ThemeProvider
           attribute="class"
