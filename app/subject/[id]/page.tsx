e99dd378ccc34214d0b4e4b9ee7cbd944d1baed1
Noah Streller <noah.streller@gmx.ch>
2024-07-06 01:54:50 +0200

    feat: better not found modal

--- app/subject/[id]/page.tsx
+++ app/subject/[id]/page.tsx
@@ -5,6 +5,8 @@ import { GradeOverviewForSubject } from "@/components/cards/grade-overview";
 import { GradesForSubject } from "@/components/cards/gradesForSubject/gradesForSubject";
 import { RequiredGradesForSubject } from "@/components/cards/required-grades";
 import { usePreferences } from "@/components/preferences-provider";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -25,6 +27,7 @@ import { doesGradePass } from "@/lib/services/notAsyncLogic";
 import { getSubjectById } from "@/lib/services/subject-service";
 import { truncateText } from "@/lib/utils";
 import { Average } from "@/types/types";
+import { Bird, HomeIcon } from "lucide-react";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -96,6 +99,27 @@ export function SubjectDetails({ subjectId }: { subjectId: string }) {
   }, [subjectId, router, session]);
 
   if (session.status === "unauthenticated") return <>Log in first lil bro</>;
+  if (dataState === "notfound")
+    return (
+      <div>
+        <Alert>
+          <Bird className="h-4 w-4" />
+          <AlertTitle className="font-bold">Woops.</AlertTitle>
+          <AlertDescription className="flex flex-col gap-4">
+            <>We cannot find the subject you are looking for.</>
+            <Button variant={"outline"} asChild>
+              <Link
+                href={"/"}
+                className="flex flex-row gap-2 justify-center items-center"
+              >
+                <HomeIcon className="size-4 text-muted-foreground" />
+                Return home
+              </Link>
+            </Button>
+          </AlertDescription>
+        </Alert>
+      </div>
+    );
   if (
     session.status === "loading" ||
     dataState === "loading" ||
@@ -130,70 +154,72 @@ export function SubjectDetails({ subjectId }: { subjectId: string }) {
       </>
     );
   if (session.status === "authenticated")
-    return dataState === "loaded" ? (
-      <CardBoard row={isDesktop}>
-        {subject ? (
-          <>
-            <CardBoard>
-              <Card>
-                <CardHeader>
-                  <CardTitle>{truncateText(subject.name!, 20).text}</CardTitle>
-                  <CardDescription>Subject details</CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <DetailRow title="ID" value={subject.id} />
-                  <DetailRow
-                    title="Name"
-                    value={truncateText(subject.name!, 40).text}
-                  />
-                  <DetailRowBoolean
-                    variant="yes-no"
-                    title="Passing"
-                    value={doesGradePass(
-                      average.gradeAverage!,
-                      userPreferences.preferences!
-                    )}
-                  />
-                  <DetailRow title="Grade count" value={average.gradeAmount} />
-                  <DetailRow
-                    title="Grade count (weights considered)"
-                    value={average.gradeWeightedAmount}
-                  />
-                  <DetailRow title="Grade sum" value={average.gradeSum} />
-                  <DetailRow
-                    title="Grade sum (weights considered)"
-                    value={average.gradeWeightedSum}
-                  />
-                </CardContent>
-              </Card>
-              <GradesForSubject
-                data={grades}
-                setData={setGrades}
-                refresh={fetchData}
-                subject={subject}
-              />
-            </CardBoard>
-            {average && (
-              <>
-                <GradeOverviewForSubject data={grades} averageData={average} />
-                <RequiredGradesForSubject
+    return (
+      dataState === "loaded" && (
+        <CardBoard row={isDesktop}>
+          {subject ? (
+            <>
+              <CardBoard>
+                <Card>
+                  <CardHeader>
+                    <CardTitle>
+                      {truncateText(subject.name!, 20).text}
+                    </CardTitle>
+                    <CardDescription>Subject details</CardDescription>
+                  </CardHeader>
+                  <CardContent>
+                    <DetailRow title="ID" value={subject.id} />
+                    <DetailRow
+                      title="Name"
+                      value={truncateText(subject.name!, 40).text}
+                    />
+                    <DetailRowBoolean
+                      variant="yes-no"
+                      title="Passing"
+                      value={doesGradePass(
+                        average.gradeAverage!,
+                        userPreferences.preferences!
+                      )}
+                    />
+                    <DetailRow
+                      title="Grade count"
+                      value={average.gradeAmount}
+                    />
+                    <DetailRow
+                      title="Grade count (weights considered)"
+                      value={average.gradeWeightedAmount}
+                    />
+                    <DetailRow title="Grade sum" value={average.gradeSum} />
+                    <DetailRow
+                      title="Grade sum (weights considered)"
+                      value={average.gradeWeightedSum}
+                    />
+                  </CardContent>
+                </Card>
+                <GradesForSubject
+                  data={grades}
+                  setData={setGrades}
+                  refresh={fetchData}
                   subject={subject}
-                  averageData={average}
                 />
-              </>
-            )}
-          </>
-        ) : (
-          <div>Subject not found</div>
-        )}
-      </CardBoard>
-    ) : (
-      dataState === "notfound" && (
-        <div>
-          Subject not found lil bro
-          <br />
-          <Link href={"/"}>Go home</Link>
-        </div>
+              </CardBoard>
+              {average && (
+                <>
+                  <GradeOverviewForSubject
+                    data={grades}
+                    averageData={average}
+                  />
+                  <RequiredGradesForSubject
+                    subject={subject}
+                    averageData={average}
+                  />
+                </>
+              )}
+            </>
+          ) : (
+            <div>Subject not found</div>
+          )}
+        </CardBoard>
       )
     );
 }
