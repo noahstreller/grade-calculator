c9c6d394ac907ac1b70b9fbdfa5c0cebd1b06f82
Noah Streller <noah.streller@gmx.ch>
2024-09-24 10:22:10 +0200

    chore: dynamic locales

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,13 +1,20 @@
+import deepmerge from "deepmerge";
 import { getRequestConfig } from "next-intl/server";
+import { cookies } from "next/headers";
 
 export default getRequestConfig(async () => {
   // Provide a static locale, fetch a user setting,
   // read from `cookies()`, `headers()`, etc.
   // const locale = "en";
-  const locale = "de";
+  // const locale = "de";
+  const locale = cookies().get("locale")?.value || "en";
+
+  const userMessages = (await import(`../messages/${locale}.json`)).default;
+  const defaultMessages = (await import(`../messages/en.json`)).default;
+  const messages = deepmerge(defaultMessages, userMessages);
 
   return {
     locale,
-    messages: (await import(`../messages/${locale}.json`)).default,
+    messages,
   };
 });
