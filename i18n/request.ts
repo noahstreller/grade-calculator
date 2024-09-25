d87ef047836c4ff15964c2c8f0988151b8b62b22
Noah Streller <noah.streller@gmx.ch>
2024-09-25 17:08:04 +0200

    fix: add functional language selection

--- i18n/request.ts
+++ i18n/request.ts
@@ -3,10 +3,6 @@ import { getRequestConfig } from "next-intl/server";
 import { cookies } from "next/headers";
 
 export default getRequestConfig(async () => {
-  // Provide a static locale, fetch a user setting,
-  // read from `cookies()`, `headers()`, etc.
-  // const locale = "en";
-  // const locale = "de";
   const locale = cookies().get("locale")?.value || "en";
 
   const userMessages = (await import(`../messages/${locale}.json`)).default;
