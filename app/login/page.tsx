45abaaddbf4020e971a056da6e566c20c37fcf86
Noah Streller <noah.streller@gmx.ch>
2024-11-26 09:03:17 +0100

    a

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,6 +2,7 @@ import { SignInPageComponent } from "@/components/pages/signin";
 import { Suspense } from "react";
 
 export default function LoginPage() {
+  // test
   return (
     <Suspense>
       <SignInPageComponent />
