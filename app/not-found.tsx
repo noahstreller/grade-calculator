2e7ff9c0fc3ee1d3dc85fc7fd107f59a4bfb18e0
Noah Streller <noah.streller@gmx.ch>
2025-01-07 15:59:52 +0100

    feat: implemented custom 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+import { NotFoundPage } from "@/components/pages/not-found-page";
+
+export default function NotFound() {
+  return (
+    <>
+      <meta name="robots" content="noindex,nofollow" />
+      <NotFoundPage />
+    </>
+  );
+}
